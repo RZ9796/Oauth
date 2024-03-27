3354147a8ff5e02f6f62a989291919c8442dcfc9
RZ9796 <rushizalte2001@gmail.com>
2024-03-27 13:37:00 +0530

    dist push

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./.env" });
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    // origin: "http://localhost:5173",
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use("/api/auth", require("./routes/authRouter"));
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.once("open", () => {
